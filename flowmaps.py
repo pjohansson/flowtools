f3f792286a78f6072ad85cf4a10a56743094d984
Petter Johansson <petter.johansson@scilifelab.se>
2013-01-10 20:09:03 +0100

    New dictionary structure implemented.

--- flowmaps.py
+++ flowmaps.py
@@ -1,91 +1,20 @@
 """Contains functions that operate on density and flow field maps."""
 from pylab import hist2d
+from numpy import inf
 
-def readDensmap(filename_densmap):
-    """Reads a density map from input filename and returns positions X, Y, 
-    number of atoms N, temperature T and mass M for individual cells 
-    as arrays."""
-
-    densmap = open(filename_densmap)
-
-    X, Y, N, T, M = ([], [], [], [], [])
-
-    # Assert that first line is good header and read rest of file
-    line = densmap.readline().strip()
-
-    if line.upper().split() == ['X', 'Y', 'N', 'T', 'M']:
-        print("Reading density map '%s' ... " % filename_densmap, end = '')
-        line = densmap.readline().strip()
-
-        while (line != ''):
-            elements = line.split(' ')
-
-            if len(elements) == 5:
-                X.append(float(elements[0]))
-                Y.append(float(elements[1]))
-                N.append(int(float(elements[2])))
-                T.append(float(elements[3]))
-                M.append(float(elements[4]))
-
-            line = densmap.readline().strip()
-
-        print("Done.")
-
-    else:
-        print("No good density map: '%s'" % filename_densmap)
-    
-    densmap.close()
-
-    return (X, Y, N, T, M)
-
-def readFlowmap(filename_flowmap):
-    """Reads a flow map from input filename and returns positions X, Y, 
-    and mass flow velocity U, V for individual cells as arrays."""
-
-    flowmap = open(filename_flowmap)
-
-    X, Y, U, V = ([], [], [], [])
-
-    # Assert that first line is good header and read rest of file
-    line = flowmap.readline().strip()
-
-    if line.upper().split() == ['X', 'Y', 'U', 'V']:
-        print("Reading flow map '%s' ... " % filename_flowmap, end = '')
-        line = flowmap.readline().strip()
-
-        while (line != ''):
-            elements = line.split(' ')
-
-            if len(elements) == 4:
-                X.append(float(elements[0]))
-                Y.append(float(elements[1]))
-                U.append(float(elements[2]))
-                V.append(float(elements[3]))
-
-            line = flowmap.readline().strip()
-
-        print("Done.")
-
-    else:
-        print("No good flow map: '%s'" % filename_flowmap)
-    
-    flowmap.close()
-
-    return (X, Y, U, V)
-
-def readSystemData(input_filename, output_filename = ''):
-    """If a precalculated data file is supplied, reads and returns total 
+def readSystem(system, data, output_filename = ''):
+    """If a precalculated data file is supplied, reads and stores total 
     number of cells N, as well as number of cells [Nx, Ny], cell dimensions
     [size_x, size_y] and system displacement [disp_x, disp_y] in each 
-    direction as a tuple.
+    direction in system dictionary.
     
     The function can also read and save this data to a file by instead 
     supplying a density map as the first argument, and a file to save this 
-    data to in the second. The data will still be returned."""
+    data to in the second. The data will still be stored."""
 
     if output_filename == '':
-        print("Reading data file '%s' ... " % input_filename , end = '') 
-        data, success = readDatafile(input_filename)
+        print("Reading data file '%s' ..." % system['datafile'] , end = ' ') 
+        success = readDatafile(system)
         if success == 1:
             print("Done.")
         else:
@@ -96,121 +25,91 @@ def readSystemData(input_filename, output_filename = ''):
         data = (0, [], [], [])
         print("Reading density map '%s' for data ... " % input_filename, 
                 end = '')
-        # data, error = readDataFromDensmap(input_filename)
         print("Done.")
         print("Saving data to file '%s' ... " % output_filename, end = '')
-        # error = saveDataFile(data, output_filename)
         print("Done.")
 
-    return data
+def readDatafile(system):
+    """Reads a data file and stores it in system. Returns a success flag."""
 
-def readDatafile(data_filename):
-    """Reads a data file and returns data and a success flag."""
+    datafile = open(system['datafile'], 'r')
 
-    datafile = open(data_filename, 'r')
+    data_list = {'numcellstotal', 'numcells', 'celldimensions', 
+            'initdisplacement'}
+
+    # Reset read data
+    for data in data_list:
+        if data in system.keys():
+            del(system[data])
 
     line = datafile.readline().strip()
-    success = 0
     while (line != ''):
-        field, sign, *values = line.split()
+        data, sign, *values = line.split()
 
-        if field == 'numcellstotal':
-            N_total = int(values[0])
-            success += 1
-
-        elif field == 'numcells':
-            N_x, N_y = map(int, values)
-            success += 1
-
-        elif field == 'celldimensions':
-            size_x, size_y = map(float, values)
-            success += 1
-
-        elif field == 'initdisplacement':
-            disp_x, disp_y = map(float, values)
-            success += 1
+        if data in data_list:
+            system[data] = []
+            for var in values:
+                system[data].append(float(var))
 
         line = datafile.readline().strip()
+
     datafile.close()
 
-    if success == 4:
+    # Check if fully read
+    if data_list <= system.keys():
+        system['numcellstotal'][0] = int(system['numcellstotal'][0])
+        system['numcells'][0] = int(system['numcells'][0])
+        system['numcells'][1] = int(system['numcells'][1])
+
         flag = 1
     else:
         flag = 0
 
-    return ((N_total, [N_x, N_y], [size_x, size_y], [disp_x, disp_y]), flag)
+    return flag
 
-def drawTemperature(X, Y, T, data, Tmin = 1, Tmax = None):
+def drawTemperature(densmap, system, Tmin = 1, **kwargs):
     """Draws the temperature array T corresponding to cell positions
     X and Y in a figure using a weighted 2d histogram and data for number
-    of bins. Can additionally be called with minimum and maximum temperature
-    to be shown."""
+    of bins. Can additionally be called with minimum temperature as cmin 
+    (default is 1)."""
 
-    hist2d(X, Y, weights = T, bins = data[1], cmin = Tmin, cmax = Tmax)
+    hist2d(densmap['X'], densmap['Y'], weights = densmap['T'], 
+            bins = system['numcells'], cmin = Tmin, **kwargs)
 
-def drawDensity(X, Y, M, data):
-    """Draws the mass density array M corresponding to cell positions
-    X and Y in a figure using a weighted 2d histogram and data for number
-    of binds."""
+def cutMap(map_to_cut, fields_to_cut, **kwargs):
+    """Cuts given fields for positions inside, specified by giving arrays
+    using keywords 'cutw' and 'cuth' for width and height respectively. 
+    Fields are given as a set."""
 
-    hist2d(X, Y, weights = M, bins = data[1])
+    opts = {'cutw' : [-inf, inf], 'cuth' : [-inf, inf]}
 
-def cutMap(width_cut, height_cut, X, Y, *fields):
-    """Cuts given fields for positions inside specified width and height.
-    The input is two vectors with boundaries in height and width respectively.
-    If min and max is equal no cut is made."""
+    print("Trying to cut fields ...", end = ' ', flush = True)
 
-    X_keep, Y_keep= ([], []) 
-    fields_keep = []
-    for i in fields:
-        fields_keep.append([])
-    
-    print("Trying to cut fields ... ", end = '')
+    parseKwars(opts, kwargs)
 
-    # Check which dimensions to cut
-    boolHeight = 0
-    if height_cut[0] < height_cut[1]:
-        boolHeight = 1
-    elif height_cut[0] > height_cut[1]:
-        boolHeight = -1
-
-    boolWidth = 0
-    if width_cut[0] < width_cut[1]:
-        boolWidth = 1
-    elif width_cut[0] > width_cut[1]:
-        boolWidth = -1
-
-    if boolHeight == -1 or boolWidth == -1:
+    # Assert that cut area is positive
+    if opts['cutw'][0] > opts['cutw'][1] or opts['cuth'][0] > opts['cuth'][1]:
         print("Cannot cut negative space!")
-        return (X_keep, Y_keep, fields_keep)
+        return 
 
-    for i in range(len(X)):
-        # Check if inside height
-        if not boolHeight:
-            boolKeep = 1
-        elif boolHeight and height_cut[0] < Y[i] < height_cut[1]:
-            boolKeep = 1
-        else:
-            boolKeep = 0
+    # 'X' and 'Y' implicit to cut
+    for field in ('X', 'Y'):
+        fields_to_cut.add(field)
 
-        # Check if inside width
-        if boolKeep == 1 and not boolWidth:
-            boolKeep = 1
-        elif boolKeep == 1 and boolWidth and width_cut[0] < X[i] < width_cut[1]:
-            boolKeep = 1
-        else:
-            boolKeep = 0
+    # Create new fields to keep
+    keep = {field : [] for field in fields_to_cut}
 
-        # If both good, append to keep
-        if boolKeep == 1:
-            X_keep.append(X[i])
-            Y_keep.append(Y[i])
-            for j in range(len(fields)):
-                fields_keep[j].append(fields[j][i])
+    for i in range(len(map_to_cut['X'])):
+        # Check if inside
+        if opts['cutw'][0] < map_to_cut['X'][i] < opts['cutw'][1] \
+                and opts['cuth'][0] < map_to_cut['Y'][i] < opts['cuth'][1]:
+            for field in fields_to_cut:
+                keep[field].append(map_to_cut[field][i])
+
+    for field in (fields_to_cut):
+        map_to_cut[field] = keep[field]
 
     print("Done.")
-
-    return (X_keep, Y_keep, fields_keep)
 
 def unmodViscFlow(X, Y, U, V, data, filename_viscdata, 
         cut_height = [-1.0, -1.0]):
@@ -232,29 +131,45 @@ def unmodViscFlow(X, Y, U, V, data, filename_viscdata,
     # Divide by unmodified flow
     # Output
 
-def constructName(filename_densbase, filename_flowbase, frame, 
-        extension = '.dat', separator = '_', num_digits = 5):
-    """Constructs filenames of density and flow maps from given bases and 
-    frame number. Optionally an extension, a separator between base and 
-    frame number and a specific number of zeros for the number can 
-    be supplied."""
+def advanceFrame(system, densmap, flowmap, frame_stride = 1, **kwargs):
+    """Advances the frame number by frame_stride (default is one frame)
+    and updates filenames to read. Does not read new maps. Does warn if
+    no files found for new frame."""
 
-    frame_filename = ('%0' + ('%d' % num_digits) + 'd') % frame
-    filename_densmap = '%s%c%s%s' % (
-            filename_densbase, separator, frame_filename, extension)
-    filename_flowmap = '%s%c%s%s' % (
-            filename_flowbase, separator, frame_filename, extension)
+    new_frame = system['frame'] + frame_stride
+    new_densfn, new_flowfn = constructFilename(system, new_frame, **kwargs)
+    print(new_densfn, new_flowfn)
+
+def parseKwars(opts, kwargs):
+    """Gives warning if an input_kwarg does not exist in avail_kwargs."""
+    for arg in kwargs.keys():
+        if arg in opts.keys():
+            opts[arg] = kwargs[arg]
+
+def constructFilename(system, frame, **kwargs): 
+    """Constructs filenames of density and flow maps from given bases and 
+    frame number. In **kwargs a separator can be set using 'sep', extension
+    using 'ext' and number of zeros 'numd'."""
+
+    opts = {'ext' : '.dat', 'sep' : '_', 'numd' : 5}
+
+    parseKwars(opts, kwargs)
+
+    frame_filename = ('%0' + ('%d' % opts['numd']) + 'd') % frame
+    filename_densmap = '%s%s%s%s' % (
+            system['densbase'], opts['sep'], frame_filename, opts['ext'])
+    filename_flowmap = '%s%s%s%s' % (
+            system['flowbase'], opts['sep'], frame_filename, opts['ext'])
 
     return (filename_densmap, filename_flowmap)
 
 def resetFields(data, fields_reset):
     """Resets specified fields in dictionary data."""
     
-    for field, values in data.items():
-        if field in fields_reset:
-            data[field] = []
+    for field in fields_reset:
+        data[field] = []
 
-def newReadDensmap(densmap_data):
+def readDensmap(densmap_data):
     """Reads a density map and stores values in dictionary."""
 
     densmap = open(densmap_data['filename'])
@@ -291,3 +206,37 @@ def newReadDensmap(densmap_data):
         densmap_data['read'] = 0
     
     densmap.close()
+
+def readFlowmap(flowmap_data):
+    """Reads a flow map and stores values in dictionary."""
+
+    flowmap = open(flowmap_data['filename'])
+
+    fields = ['X', 'Y', 'U', 'V']
+    resetFields(flowmap_data, fields)
+
+    # Assert that first line is good header and read rest of file
+    header = flowmap.readline().strip().upper().split()
+
+    if header == fields:
+        print("Reading flow map '%s' ..." 
+                % flowmap_data['filename'], end = ' ', flush = True)
+
+        line = flowmap.readline().strip()
+        while (line != ''):
+            values = line.split()
+
+            if len(values) == 4:
+                for i, value in enumerate(values):
+                    flowmap_data[fields[i]].append(float(value))
+
+            line = flowmap.readline().strip()
+
+        print("Done.")
+        flowmap_data['read'] = 1
+
+    else:
+        print("No good flow map: '%s'" % flowmap_data['filename'])
+        flowmap_data['read'] = 0
+    
+    flowmap.close()
