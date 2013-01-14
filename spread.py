e635feac29bb113a88b24c8f1134687e7f04e526
Petter Johansson <petter.johansson@scilifelab.se>
2013-01-14 14:34:28 +0100

    Plotting works, more testing needed.

--- spread.py
+++ spread.py
@@ -1,7 +1,10 @@
 """Module containing tool to plot spreading as a function of time."""
 
-from util import construct_filename, parse_kwargs
-from pyplot import plot
+from util import construct_filename, parse_kwargs, reset_fields
+from numpy import floor
+from pylab import plot, xlabel, ylabel, title
+from datamaps import read_flowmap
+from rows import find_cells_in_row, keep_droplet_cells_in_row, find_edges_in_row
 
 def plot_spread_vs_time(system, frames, **kwargs):
     """Finds the spread of the contact line of a droplet and plots it for a 
@@ -10,26 +13,33 @@ def plot_spread_vs_time(system, frames, **kwargs):
     
     A specific flow map filename structure can be specified using **kwargs
     as for construct_filename. **kwargs can also be used to specify a row in
-    which the spreading is counted as row = 'num."""
+    which the spreading is counted as row = 'num', to cut the flow map at
+    a height using cut = 'height' and to save the contact line information
+    into an external dictionary as line = contact_line."""
 
-    contact_line = {}
+    opts = {'line' : {}}
+    parse_kwargs(opts, kwargs)
+
+    contact_line = opts['line']
+
     find_contact_line_spread(contact_line, system, frames, **kwargs)
     trim_empty_head(contact_line, frames)
 
     if 'delta_t' in system.keys():
-        time = []
-        for i, value in enumerate(frames):
-            time[i] = i * system['delta_t']
-
-        draw_spread_time(contact_line, time)
+        draw_spread_time(contact_line, system)
 
     return None
 
-def draw_spread_time(contact_line, time, **kwargs):
+def draw_spread_time(contact_line, system, **kwargs):
     """Draws a plot of a spreading droplet on a surface as a function of
     time. Plotting commands can be set using **kwargs."""
 
-    plot(contact_line['spread'], frames)
+    contact_line['times'] = []
+    frame_null = contact_line['frames'][0]
+    for i, frame in enumerate(contact_line['frames']):
+        contact_line['times'].append((frame - frame_null) * system['delta_t'])
+
+    plot(contact_line['times'], contact_line['spread'])
     xlabel('Time (ps)'); ylabel('Spread of contact line (nm)');
     title('Spread of contact line of droplet on a quadrupole surface.')
 
@@ -48,14 +58,16 @@ def find_contact_line_spread(contact_line, system, frames, **kwargs):
     densmap = {}; flowmap = {};
 
     opts = {'row' : None}
-    parse_kwargs(opts, {'row'})
+    parse_kwargs(opts, kwargs)
 
     if opts['row'] == None:
-        row = floor((system['floor'] - system['initdisplacement'][1])
-                % system['celldimensions'][1]) - 1
+        row = int(floor((system['floor'] - system['initdisplacement'][1])
+                / system['celldimensions'][1]))
         opts['row'] = row
 
     row = {'num' : opts['row']}
+
+    reset_fields(contact_line, {'spread', 'frames'})
 
     for system['frame'] in frames:
         densmap['filename'], flowmap['filename'] = \
@@ -66,7 +78,7 @@ def find_contact_line_spread(contact_line, system, frames, **kwargs):
         keep_droplet_cells_in_row(row, flowmap, system)
         find_edges_in_row(row, system)
 
-        contact_line['frame'].append(system['frame'])
+        contact_line['frames'].append(system['frame'])
         if row['edges'] != []:
             contact_line['spread'].append(row['edges'][1] - row['edges'][0])
         else:
@@ -79,9 +91,8 @@ def trim_empty_head(contact_line, frames):
     and trims frames to fit from back. The remainder is a contact_line with
     values starting from first impact and frames counting from that point."""
 
-    while contact_line['spread'][0] == 0:
-        for field in contact_line.keys():
-            del(contact_line[field][0])
-        del(frames[-1])
+    while contact_line['spread'] != [] and contact_line['spread'][0] == 0:
+        del(contact_line['spread'][0])
+        del(contact_line['frames'][-1])
 
     return None
