b173c07cb622c60b405351118fe302d5737737dc
Petter Johansson <petter.johansson@scilifelab.se>
2013-01-14 15:04:20 +0100

    Minor modifications.

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 """Contains tools for main tasks on flow maps."""
 
-def plot_flowmaps(system = {}, frames, **kwargs):
+from util import construct_filename
+
+def plot_flowmaps(system, frames, **kwargs):
     """Draws quiver plots of flow maps for specified frames. If no system
     supplied as input, or information lacking, asks user for file to 
     read from. By supplying options as for cut_maps in **kwargs the system
     can be cut before outputting. If so a density map has to be supplied for
     reconfiguration to succeed."""
+    flowmap = {}
 
-    for system['frame'] in frames:
-        
+    for frame in frames:
+        construct_filename(flowmap, system['flowbase'], frame)
