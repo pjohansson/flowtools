c3545518cdc96bfb25df05610af2ab37f3c6162b
Petter Johansson <petter.johansson@scilifelab.se>
2013-01-17 16:02:40 +0100

    Density works, explore more options for plotting.

--- density.py
+++ density.py
@@ -1,19 +1,25 @@
 from cut_system import cut_map
-from pylab import axis, clf, colorbar, figure, hist2d
+from numpy import inf
+from pylab import axis, clf, colorbar, figure, hist2d, title, xlabel, ylabel, xlim, ylim
 from read_data import read_densmap
 from save_data import save_figure
-from util import construct_filename, parse_kwargs
+from util import construct_filename, parse_kwargs, reset_fields
 
-def plot_density(system, frames, save_to = None, **kwargs):
+def plot_density(system, frames, **kwargs):
     """Calls relevant functions to draw energy maps of supplied density maps.
-    Options for cut_map and hist2d can be supplied as usual through **kwargs.
+    Options for cut_map and draw_density can be supplied as usual through 
+    **kwargs.
 
-    To output frame images to .png, enter a base name for save_to."""
+    To output frame images to .png, **kwargs can be used to enter a base name 
+    with save_to and dpi setting with dpi."""
 
     densmap = {}
 
-    opts = {'cutw' : [-inf, inf], 'cuth' : [-inf, inf]}
+    opts = {'cutw' : [-inf, inf], 'cuth' : [-inf, inf], 
+            'save_to' : None, 'dpi' : 200}
     parse_kwargs(opts, kwargs)
+
+    find_normalising_factor(system, frames)
 
     clf()
 
@@ -22,12 +28,12 @@ def plot_density(system, frames, save_to = None, **kwargs):
         read_densmap(densmap)
 
         cut_map(densmap, {'X', 'Y', 'N', 'T', 'M'}, system, **opts)
-        normalise_density(densmap)
-        draw_energy(densmap, system, **kwargs)
+        normalise_density(densmap, system)
+        draw_density(densmap, system, **kwargs)
 
-        if save_to != None:
-            save_figure(save_to_base, frame)
-        else:
+        if opts['save_to'] != None:
+            save_figure(opts['save_to'], frame, opts['dpi'])
+        elif frame < frames[-1]:
             figure()
 
     return None
@@ -62,17 +68,31 @@ def normalise_density(densmap, system):
     return None
 
 def draw_density(densmap, system, **kwargs):
-    """Draws the energy array E corresponding to cell positions
-    X and Y in a figure using a weighted 2d histogram and data for number
-    of bins. Can additionally be called with minimum energy as Emin 
-    (default is 1)."""
+    """Draws the energy array E corresponding to cell positions X and Y in 
+    a figure using a weighted 2d histogram and data for number of bins. 
+    Normally caps colormap for normalised density values between 0 and 1, this 
+    can be modified by supplying different vmin and vmax as kwargs, disabling
+    by using None. 
+    
+    Supplying minimum and maximum masses to show can be done similarly as 
+    mmin and mmax, these are normalised by system['max_mass']. Other **kwargs
+    as for hist2d."""
 
-    opts = {'colormap' : True}
+    opts = {'colormap' : True, 'mmin' : -inf, 'mmax' : inf, 
+            'vmin' : 0.0, 'vmax' : 1.0}
     parse_kwargs(opts, kwargs)
 
+    opts['mmin'] /= system['max_mass']
+    opts['mmax'] /= system['max_mass']
+
     hist2d(densmap['X'], densmap['Y'], weights = densmap['dens_norm'], 
-            bins = system['numcells'], **kwargs)
-    axis('equal')
+            bins = system['numcells'], vmin = opts['vmin'], vmax = opts['vmax'],
+            cmin = opts['mmin'], cmax = opts['mmax'], **kwargs)
+    axis('scaled');
+
+    title('Density of droplet on substrate.')
+    xlabel('Position (nm)')
+    ylabel('Height (nm)')
     
     if opts['colormap']:
         colorbar()
