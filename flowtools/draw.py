548a34013ab04913c59d057e5cb96ee65d93b5e5
Petter Johansson <petter.johansson@scilifelab.se>
2013-02-25 23:12:12 +0100

    Decorator for drawing implemented.

---
+++ flowtools/draw.py
@@ -0,0 +1,45 @@
+import pylab as plt
+
+def draw(func):
+    """Decorator for giving common plot options."""
+
+    def wrapper(**kwargs):
+        # Read options
+        axis = kwargs.pop('axis', 'scaled')
+        colorbar = kwargs.pop('colorbar', False)
+        legend = kwargs.pop('legend', False)
+        save = kwargs.pop('save', '')
+        show = kwargs.pop('show', False)
+        xlim = kwargs.pop('xlim', None)
+        ylim = kwargs.pop('ylim', None)
+
+        # Open figure
+        plt.figure()
+
+        # Labels
+        plt.xlabel(kwargs.pop('xlabel', 'Position (nm)'))
+        plt.ylabel(kwargs.pop('ylabel', 'Height (nm)'))
+        plt.title(kwargs.pop('title', ''))
+
+        # Drawing function called with remaining keywords
+        func(**kwargs)
+
+        # View
+        plt.axis(axis)
+        plt.xlim(xlim)
+        plt.ylim(ylim)
+
+        # Decorating options
+        if legend:
+            plt.legend()
+        if colorbar:
+            plt.colorbar()
+
+        # Show
+        if show:
+            plt.show()
+
+        # Save
+        if save:
+            plt.savefig(save)
+    return wrapper
