83110461faab8d18f661a5260c89a739c5755923
Petter Johansson <petter.johansson@scilifelab.se>
2013-03-06 16:37:59 +0100

    savespread.py rewritten

--- savespread.py
+++ savespread.py
@@ -1,58 +1,46 @@
 #!/usr/bin/python
 # Calculates spread for files in a directory
 
-import os
-import sys
+import argparse
+import numpy as np
 
-from flowtools.datamaps import create_filenames, System
-from flowtools.spread import Spread
+from flowtools.datamaps import System
 
-try:
-    base = sys.argv[1]
-    floor = int(sys.argv[2])
-    save = sys.argv[3]
-except Exception:
-    print("Usage: %s %s %s %s %s"
-            % (sys.argv[0], 'base', 'floor', 'save',
-                '[delta_t [[min_mass [start [end]]]]'))
-    exit()
+parser = argparse.ArgumentParser()
 
-try:
-    delta_t = float(sys.argv[4])
-except Exception:
-    delta_t = 0.
+# Required arguments
+parser.add_argument('base', help="file name base of system")
+parser.add_argument('floor', help="floor of the system", type=int)
+parser.add_argument('save', help="file for saving spread data to")
 
-try:
-    min_mass = float(sys.argv[5])
-except Exception:
-    min_mass = 0.
+# Optional arguments
+parser.add_argument('-dt', '--delta_t', type=float, default=0.,
+        help="time difference between frames")
+parser.add_argument('-m', '--min_mass', type=float, default=0.,
+        help="minimum mass of droplet cells")
+parser.add_argument('-s', '--start', type=int, default=1,
+        help="initial frame number")
+parser.add_argument('-e', '--end', type=int, default=np.inf,
+        help="final frame number")
+parser.add_argument('-rel', '--relative', action='store_true',
+        help="save to filename in same base directory as files")
 
-try:
-    start = int(sys.argv[6])
-except Exception:
-    start = 1
+# Parse
+args = parser.parse_args()
 
-try:
-    end = int(sys.argv[7])
-except Exception:
-    end = None
-
-system = System(delta_t = delta_t, min_mass = min_mass, floor = floor)
-
-# Try file paths
-num = start
-while os.path.isfile('%s%05d%s' % (base, num, '.dat')):
-    num += 1
-
-# Set end frame
-if end and num > end:
-    num = end + 1
+system = System(
+        base = args.base, delta_t = args.delta_t,
+        floor = args.floor, min_mass = args.min_mass
+        )
 
 # Create file names and collect spread
-system.datamaps = create_filenames(base, list(range(start, num)))
-collect = Spread(system, delta_t = delta_t, print = True)
-collect.com()
+system.files(start = args.start, end = args.end)
+spread = system.spread()
 
 # Save to file in same folder
-_file = os.path.dirname(base) + '/' + save
-collect.save(_file)
+if args.relative:
+    _file = os.path.dirname(args.base) + '/' + args.save
+else:
+    _file = args.save
+
+spread.save(_file)
