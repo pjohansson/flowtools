062103c38ab8900df7096928279eda34c9248b16
Petter Johansson <petter.johansson@scilifelab.se>
2013-03-06 17:12:44 +0100

    .gitignore fixed

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
+#!/usr/bin/env python
+
 from distutils.core import setup
 
 setup(
         name='Flowtools',
         description="Tools for flow maps from modified Gromacs simulations",
+        version='0.1',
         author="Petter Johansson",
         author_email="petter.johansson@scilifelab.se",
         packages=['flowtools'],
         requires=['numpy', 'pylab', 'scipy'],
-        scripts=['scripts/collect_spread']
+        scripts=['scripts/collect_spread.py']
         )
